be49f81f5d30c8100dc4aeea4b1bda60709920d0
Cody <ken51717@gmail.com>
2024-10-22 11:48:07 +0800

    fix :修改OG標籤

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,21 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "台灣捐血資訊整理小網站",
-  description: "一點點資訊曉整理",
+  description: "一點點資訊整理",
+  openGraph: {
+    title: "台灣捐血資訊整理小網站",
+    description: "一點點資訊整理，讓更多人了解捐血資訊的重要性",
+    url: "https://blood-info.vercel.app/", // 替換為您的網站 URL
+    type: "website",
+    images: [
+      {
+        url: "/imgs/og-img.jpg", // 替換為您要顯示的圖像 URL
+        width: 1200,
+        height: 630,
+        alt: "台灣捐血資訊整理小網站的封面圖片",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
