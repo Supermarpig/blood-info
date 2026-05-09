1a2752d35f3a5dd190929118bdbba4f41e64cdf2
Cody <ken51717@gmail.com>
2026-05-09 12:22:46 +0800

    fix: fix recent

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -57,6 +57,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/recent`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/faq`,
       lastModified: new Date(),
       changeFrequency: "weekly",
